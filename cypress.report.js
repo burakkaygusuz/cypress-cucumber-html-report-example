ca2a208930d99250532616ecc9fa3781525e61d3
Burak Kaygusuz <kaygusuzburak@gmail.com>
2023-07-03 06:54:07 +0300

    Download and execute the file according to the OS

--- cypress.report.js
+++ cypress.report.js
@@ -1,6 +1,60 @@
 const report = require('multiple-cucumber-html-reporter');
 const dayjs = require('dayjs');
 const fs = require('fs');
+const https = require('https');
+const os = require('os');
+const path = require('path');
+const { exec } = require('child_process');
+
+const BASE_URL = 'https://github.com/cucumber/json-formatter/releases/download/v19.0.0';
+const FILE_NAME = 'cucumber-json-formatter';
+
+let url = '';
+let fileName = FILE_NAME;
+
+switch (os.platform()) {
+  case 'win32':
+    url = `${BASE_URL}/${FILE_NAME}-windows-amd64`;
+    fileName += '.exe';
+    break;
+  case 'darwin':
+    url = `${BASE_URL}/${FILE_NAME}-darwin-amd64`;
+    break;
+  case 'linux':
+    url = `${BASE_URL}/${FILE_NAME}-linux-amd64`;
+    break;
+  default:
+    console.log('Unsupported platform');
+    process.exit(1);
+}
+
+const filePath = path.join('./.bin', fileName);
+fs.mkdirSync('./.bin', { recursive: true });
+const file = fs.createWriteStream(filePath);
+
+https
+  .get(url, (response) => {
+    response.pipe(file);
+
+    file.on('finish', () => {
+      file.close();
+      console.log('File downloaded and renamed successfully');
+
+      if (os.platform() === 'darwin' || os.platform() === 'linux') {
+        exec(`chmod +x ${filePath}`, (error) => {
+          if (error) {
+            console.log(`Error changing file permission: ${error}`);
+          } else {
+            console.log('File permission changed successfully');
+          }
+        });
+      }
+    });
+  })
+  .on('error', (err) => {
+    fs.unlinkSync(filePath);
+    console.log('Error: ', err.message);
+  });
 
 const runInfo = JSON.parse(fs.readFileSync('cypress/reports/results.json', 'utf8'));
 
